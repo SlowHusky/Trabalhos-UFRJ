fe2f3350ee2e73c841a187bdeb4b29a63d383cfe
SlowHusky <slowhusky@gmail.com>
2017-12-02 10:26:38 -0200

    Inicio do trabalho

---
+++ AlgGraf/lista1-1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int criar_matriz(int linhas, int colunas)
+{
+    int peso;
+    for(int i = 0; i < linhas; i++)
+        for(int j = 0; j < colunas; j++)
+            scanf("%d", )
+}
