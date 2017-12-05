83e56c12613aeb36e6a54a7d1700062784a52067
SlowHusky <slowhusky@gmail.com>
2017-12-05 18:10:02 -0200

    Fim da lista

--- AlgGraf/lista1-1.py
+++ AlgGraf/lista1-1.py
@@ -2,7 +2,7 @@
 """
 Trabalho da lista 1
 """
- #exercicio 1
+"""exercicio 1
 def criar_matriz(linhas):
     matriz = []
     for i in range(linhas):
@@ -37,3 +37,54 @@ if __name__ == "__main__":
     m1 = criar_matriz(linhas)
     imprimir_matriz(m1)
     print lista_adjacencias(m1)  
+    print("o algoritmo tem ordem n ao quadrado")
+    print("ele funciona para digrafos")
+"""    
+    
+"""
+    exercício 2:
+"""
+    
+"""
+    exercício 3:
+    Nas matrizes de adjacências sem peso, concideram que as células
+    da matriz ficam 1 quando tem ligação e zero(ou vazio) quando não 
+    existe. Nas matrizes com peso, os valores das células representam
+    os pesos entre as ligações. Nas listas de adjacências sem peso, as 
+    representações se parecem com listas encadeadas, que mostram 
+    para quem existe caminho. Nas listas de adjacências com peso, além
+    da informação de quem possue caminho, cada elemento deve mostrar
+    qual o peso deste. Ou seja, existe uma informação a mais na cadeia.
+    """
+
+"""
+    exercicio 4:
+
+    
+def contador_head_tail(n):
+    lista = []
+    contador = 0
+    for i in range(n):
+        lista.append(raw_input())
+    for i in range(int(n/2)):
+        base = lista[i]
+        for j in range(n):
+            if lista[j] != base:
+                if (lista[j][0] == base[-1] or lista[j][-1] == base[0]):
+                    print base
+                    print lista[j]
+                    contador += 1
+    if contador == 0:
+        print -1
+        
+if __name__ == "__main__":
+    print "iniciando"
+    n = input("\n entre com o dado: ")
+    contador_head_tail(n)
+
+"""
+
+"""
+    exercício 5
+"""
+
