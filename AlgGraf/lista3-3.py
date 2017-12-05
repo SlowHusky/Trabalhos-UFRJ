aa5ce79dd05ea46f71b636b028ad6d72671fe677
SlowHusky <slowhusky@gmail.com>
2017-12-05 20:00:56 -0200

    Lista 3

---
+++ AlgGraf/lista3-3.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Lista de exercícios 3
+Victor Ribeiro Pires
+113051532
+"""
+
+"""
+    exercício 1:
+
+def andar(n):
+    lista = []
+    contador = 0
+    for i in range(n):
+        lista.append(str(i+1))
+    #for i in range(n):
+        if lista[i][-1] == "4":
+            contador += 1
+    numero = int(lista[-1])
+    resultado = numero + contador
+    if (str(resultado) == "4"):
+        resultado += 1
+        print resultado
+        return resultado
+    print resultado
+    return resultado
+    
+
+if __name__ == "__main__":
+    n = input()
+    andar(n)
+"""
+
+"""
+   # exercício 2
+"""
+
+def relacionamento(n, m, q):
+    
+
+if __name__ == "__main__":
+    n = input()
+    m = input()
+    q = input()
+    print "go"
